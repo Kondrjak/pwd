148593850f283e051a00346400429f9fe9f78b80
RomsenKabomsen <kondrjakow@posteo.de>
2021-09-10 15:47:55 +0200

    Destroyed some tests

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -2,7 +2,7 @@ public class Password {
     String password;
     boolean valid;
 
-    static int minLen = 6;
+    static int minLen = 8;
     static int maxLen = 32;
 
     static char[] abc = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
